d691d0c92add12b08c84c1543806cbc487ee1e66
NIcholas Kneafsey <nicholaskneafsey@gmail.com>
2019-08-31 18:50:45 -0700

    Mostly fix everything. wait for expo react-native version that uses 0.60.x

--- src/components/ServicesScreen.js
+++ src/components/ServicesScreen.js
@@ -1,51 +1,55 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { ScrollView, Image }  from 'react-native'
-import { List, Text, ListItem } from 'react-native-elements'
-import _ from 'lodash'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ScrollView, Image } from "react-native";
+import { List, Text, ListItem } from "react-native-elements";
+import _ from "lodash";
 
-import { getOrCreateAuthToken } from '../utilities/Auth'
-import services from '../utilities/services'
-import { updateSelectedService } from '../actions/ServiceActions'
+import { getOrCreateAuthToken } from "../utilities/Auth";
+import services from "../utilities/services";
+import { updateSelectedService } from "../actions/ServiceActions";
 
 class ServicesScreen extends Component {
   static navigationOptions = {
-    title: 'Services'
-  }
+    title: "Services"
+  };
 
   componentWillMount() {
     // check for auth token and make request to auth endpoint if not
-    getOrCreateAuthToken()
+    getOrCreateAuthToken();
   }
 
   render() {
     return (
       <ScrollView>
         <List>
-          {
-            services.filter((service) => service.developerAssociate).map((service) => {
+          {services
+            .filter(service => service.developerAssociate)
+            .map(service => {
               return (
                 <ListItem
                   key={service.key}
                   title={service.value}
                   avatar={{ uri: service.imageUrl }}
                   avatarStyle={{
-                    backgroundColor: 'white',
-                    resizeMode: Image.resizeMode.contain  // add this back later
+                    backgroundColor: "white",
+                    resizeMode: "contain" // add this back later
                   }}
                   onPress={() => {
-                    this.props.updateSelectedService(service.key)
-                    this.props.navigation.navigate('Service', { service: service.key })
+                    this.props.updateSelectedService(service.key);
+                    this.props.navigation.navigate("Service", {
+                      service: service.key
+                    });
                   }}
                 />
-              )
-            })
-          }
+              );
+            })}
         </List>
       </ScrollView>
-    )
+    );
   }
 }
 
-
-export default connect(null, { updateSelectedService })(ServicesScreen)
+export default connect(
+  null,
+  { updateSelectedService }
+)(ServicesScreen);
